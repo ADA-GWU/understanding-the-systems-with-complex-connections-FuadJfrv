7578a636bad4c7d138901d72e57746d0efc5a0ec
FuadJfrv <fjafarov11530@ada.edu.az>
2023-09-24 21:25:08 +0400

    Testing out client server communication with two ports

--- Client and Software App/Client.java
+++ Client and Software App/Client.java
@@ -5,30 +5,43 @@ import java.util.Scanner;
 
 public class Client
 {
-    public static void main(String[]args)
+    public static void main(String[]args) throws IOException
     {
-        try
-        {
-            Socket socket = new Socket("localhost", 9001);
+        Socket socket = new Socket("localhost", 9001);
+        Socket socket2 = new Socket("localhost", 9002);
 
-            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println("Enter a number: ");
+        BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
 
-            float num = Float.parseFloat(inputReader.readLine());
-            socketWriter.println(num);
+        BufferedReader socket2Reader = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
+        PrintWriter socket2Writer = new PrintWriter(socket2.getOutputStream(), true);
 
-            System.out.println(socketReader.readLine());
 
-            socket.close();
-            socketReader.close();
-            socketWriter.close();
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
+
+        System.out.println("Enter a number: ");
+
+        float num = Float.parseFloat(inputReader.readLine());
+        socketWriter.println(num);
+
+        System.out.println(socketReader.readLine());
+
+
+        System.out.println("Enter a number: ");
+
+        float num2 = Float.parseFloat(inputReader.readLine());
+        socket2Writer.println(num2);
+
+        System.out.println(socket2Reader.readLine());
+
+
+
+        //Close IO
+        socket.close();
+        socket2.close();
+        socketReader.close();
+        socketWriter.close();
     }
+
 }
--- Client and Software App/Server.java
+++ Client and Software App/Server.java
@@ -7,31 +7,43 @@ import java.net.Socket;
 
 public class Server
 {
-    public static void main(String[] args)
+    static int port = 9000;
+    public static void main(String[] args) throws IOException
     {
-        try
-        {
-            ServerSocket serverSocket = new ServerSocket(9001);
+        System.out.println("Enter a unique port number: ");
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+        port = Integer.parseInt(inputReader.readLine());
 
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected");
+        ServerSocket serverSocket = new ServerSocket(port);
+        ServerSocket serverSocket2 = new ServerSocket(9002);
 
-            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
+        System.out.println("Waiting for client ");
+        Socket socket = serverSocket.accept();
+        System.out.println("Client connected");
+
+        System.out.println("Waiting for client 2");
+        Socket socket2 = serverSocket2.accept();
+        System.out.println("Client 2 connected");
+
+        BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
+
+        BufferedReader socket2Reader = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
+        PrintWriter socket2Writer = new PrintWriter(socket2.getOutputStream(), true);
 
 
-            float num = Float.parseFloat(socketReader.readLine());
-            socketWriter.println("Server says: " + num * 2);
+        float num = Float.parseFloat(socketReader.readLine());
+        socketWriter.println("Server says: " + num * 2);
+
+        float num2 = Float.parseFloat(socket2Reader.readLine());
+        socket2Writer.println("Server says: " + num2 * 3);
 
 
-            serverSocket.close();
-            socket.close();
-            socketReader.close();
-            socketWriter.close();
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
+
+        //Close IO
+        serverSocket.close();
+        socket.close();
+        socketReader.close();
+        socketWriter.close();
     }
 }
