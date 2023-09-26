de615bf00f6fce4708eecb5922685f010b5f2e39
FuadJfrv <fjafarov11530@ada.edu.az>
2023-09-26 13:49:38 +0400

    Better client code
    Improved the code redundancy in client class.
    Currently can only can do 1 client and 2 servers for simplicity.
    Also need to add a loop so client can send as many messages as he wants.

--- Client and Software App/Client.java
+++ Client and Software App/Client.java
@@ -1,23 +1,26 @@
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client
 {
+    private static ArrayList<Socket> sockets = new ArrayList<>();
+    //private static Socket sockets[];
     public static void main(String[]args) throws IOException
     {
         Socket socket = new Socket("localhost", 9001);
         Socket socket2 = new Socket("localhost", 9002);
+        
+        sockets.add(socket);
+        sockets.add(socket2);
 
-        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+        /*BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+
 
         BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
-
-        BufferedReader socket2Reader = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
-        PrintWriter socket2Writer = new PrintWriter(socket2.getOutputStream(), true);
-
 
 
         System.out.println("Enter a number: ");
@@ -25,21 +28,34 @@ public class Client
         float num = Float.parseFloat(inputReader.readLine());
         socketWriter.println(num);
 
-        System.out.println(socketReader.readLine());
+        System.out.println(socketReader.readLine()); */
 
+
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Enter a number: ");
+        float num = Float.parseFloat(inputReader.readLine());
 
-        float num2 = Float.parseFloat(inputReader.readLine());
-        socket2Writer.println(num2);
+        System.out.println("Enter a server index: ");
+        Integer socketInd = Integer.parseInt(inputReader.readLine());
 
-        System.out.println(socket2Reader.readLine());
-
-
+        CallServer(socketInd, num);
 
         //Close IO
         socket.close();
-        socket2.close();
+        //socketReader.close();
+        //socketWriter.close();
+    }
+
+    private static void CallServer(Integer socketIndex, Float numberToDouble) throws IOException
+    {
+        BufferedReader socketReader = new BufferedReader(new InputStreamReader(sockets.get(socketIndex).getInputStream()));
+        PrintWriter socketWriter = new PrintWriter(sockets.get(socketIndex).getOutputStream(), true);
+
+        socketWriter.println(numberToDouble);
+
+        System.out.println(socketReader.readLine());
+
         socketReader.close();
         socketWriter.close();
     }
--- Client and Software App/Server.java
+++ Client and Software App/Server.java
@@ -10,34 +10,23 @@ public class Server
     static int port = 9000;
     public static void main(String[] args) throws IOException
     {
-        System.out.println("Enter a unique port number: ");
+        System.out.println("Enter a 9001, 9002: ");
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
         port = Integer.parseInt(inputReader.readLine());
 
         ServerSocket serverSocket = new ServerSocket(port);
-        ServerSocket serverSocket2 = new ServerSocket(9002);
 
         System.out.println("Waiting for client ");
         Socket socket = serverSocket.accept();
         System.out.println("Client connected");
 
-        System.out.println("Waiting for client 2");
-        Socket socket2 = serverSocket2.accept();
-        System.out.println("Client 2 connected");
 
         BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter socketWriter = new PrintWriter(socket.getOutputStream(), true);
 
-        BufferedReader socket2Reader = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
-        PrintWriter socket2Writer = new PrintWriter(socket2.getOutputStream(), true);
-
 
         float num = Float.parseFloat(socketReader.readLine());
         socketWriter.println("Server says: " + num * 2);
-
-        float num2 = Float.parseFloat(socket2Reader.readLine());
-        socket2Writer.println("Server says: " + num2 * 3);
-
 
 
         //Close IO
